2645c06a104a6981b5ee3c6dec2e58b48b490d83
ZakS182 <zakstersaeed@gmail.com>
2024-09-06 14:54:20 -0400

    Used linspace instead of arange, added comments

--- ps-1/plot_gaussian.py
+++ ps-1/plot_gaussian.py
@@ -1,17 +1,26 @@
 import numpy as pn
 import matplotlib.pyplot as plt
 
-def gaussian(X, stdev):
-    return (1/(stdev * pn.sqrt(2 * pn.pi))) * pn.exp(-(X/(2 * stdev)) ** 2)
+'''
+Set our standard deviation (and the Gaussian normalisation accordingly).
+Set up equally spaced X-values and the Gaussian function by which we get the corresponding Y-values.
+Plot the Gaussian, set appropriate x-limits, show x=0 axis, and format plot and axes titles.
+Finally, save the figure and then show it.
+'''
+sigma = 3
+norm = (1/(sigma * pn.sqrt(2 * pn.pi)))
 
-x = pn.arange(-10, 10, 0.1, dtype=pn.float64)
-y = gaussian(x, 3)
+def gaussian(x, N, stdev):
+    return N * pn.exp(-(x/(2 * stdev)) ** 2)
 
-plt.plot(x, y)
+X = pn.linspace(-10, 10, 100, dtype=pn.float64)
+Y = gaussian(X, norm, sigma)
+
+plt.plot(X, Y)
 plt.xlim(-10, 10)
-plt.ylim(0, pn.max(y) * 1.2)
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.title("Normalised Gaussian with $\sigma = 3$")
+plt.axvline(x=0, color='grey')
 plt.savefig("gaussian.png")
 plt.show()
