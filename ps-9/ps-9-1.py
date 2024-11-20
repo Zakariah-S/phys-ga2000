1928977a758c07949247f6f09c0e16f33260ddf7
ZakS182 <zakstersaeed@gmail.com>
2024-11-20 13:22:56 -0500

    Created problem set 9 files

---
+++ ps-9/ps-9-1.py
@@ -0,0 +1,3 @@
+"""
+Exercise 8.6 in Newman.
+"""
---
+++ ps-9/ps-9-2.py
@@ -0,0 +1,8 @@
+"""
+Exercise 8.7 in Newman. 
+Before approaching the problem numerically, 
+show that set of possible values of R, ρ, C, m, and g maps to a one-parameter family of solutions, 
+which in terms of a typical timescale T is controlled by the combination R^2ρCgT^2/m. 
+That is, show that if you rescale variables from t to t′/T and from x to an appropriate x′, 
+you get a unitless set of equations with one free (unitless) parameter.
+"""
